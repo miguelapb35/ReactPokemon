6be2142dffa99ec106b2c5ae7479d4cd4de8d427
Miguel Angel <miguelapb35@gmail.com>
2020-04-07 22:38:52 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import NavBar from './components/layout/NavBar';
+import Dashboard from './components/layout/Dashboard';
+import backgroundImage from './pattern.png';
+import Pokemon from './components/pokemon/Pokemon';
+class App extends Component {
+  state = {  }
+  render() { 
+    return ( 
+      <Router>
+        <div className='App' style={{background: `url(${backgroundImage})`}}>
+          <NavBar />
+          <div className='container'>
+            <Switch>
+              <Route exact path='/' component={Dashboard} />
+              <Route exact path='/pokemon/:pokemonIndex' component={Pokemon} />
+              
+            </Switch>
+          </div>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
