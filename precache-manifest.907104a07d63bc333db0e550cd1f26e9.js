d8543e42f8338a51a23a5d5786efb93115ffaced
Miguel Angel <miguelapb35@gmail.com>
2020-04-07 22:45:03 -0500

    Updates

---
+++ precache-manifest.907104a07d63bc333db0e550cd1f26e9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00ace01cf643a29425d413cf73864f52",
+    "url": "/ReactPokemon/index.html"
+  },
+  {
+    "revision": "a4c145a093f2fb460d70",
+    "url": "/ReactPokemon/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "efede8bc562bf7bf0c16",
+    "url": "/ReactPokemon/static/css/main.9b33b499.chunk.css"
+  },
+  {
+    "revision": "a4c145a093f2fb460d70",
+    "url": "/ReactPokemon/static/js/2.f9a5bcba.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ReactPokemon/static/js/2.f9a5bcba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efede8bc562bf7bf0c16",
+    "url": "/ReactPokemon/static/js/main.7c0b8a6b.chunk.js"
+  },
+  {
+    "revision": "46ca4189e0271d762ed0",
+    "url": "/ReactPokemon/static/js/runtime-main.8c257fc4.js"
+  },
+  {
+    "revision": "5dd02025d6a3351358d0fcc54a5e6e24",
+    "url": "/ReactPokemon/static/media/spinner.5dd02025.gif"
+  }
+]);
